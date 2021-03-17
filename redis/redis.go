b189c0b06d15a7e952bf79135eaf70898ffacbf7
Sachin Kumar Rajput <sachrajput1989@gmail.com>
2021-03-17 17:38:41 +0530

    Update redis.go

--- redis/redis.go
+++ redis/redis.go
@@ -8,7 +8,7 @@ import "github.com/go-redis/redis"
 func RedisClient() *redis.Client {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "redis-12892.c252.ap-southeast-1-1.ec2.cloud.redislabs.com:12892",
-		Password: "cK1q1UALUJkEQ0kCSbP4pmgGvmjFQtYk",
+		Password: "",
 		DB:       0,
 	})
 
